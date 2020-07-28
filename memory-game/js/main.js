74b53c240674267219eda3470113e0cc0122102e
Cadavid Berns Marissa <CadavidBernsMarissa@JohnDeere.com>
2020-07-28 16:00:01 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,18 +1,25 @@
 let cards = ["queen", "queen","king","king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flpped " + cardTwo);
-
-if (cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
+function checkForMatch(){
+if(cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match!");
 	}
 	else{
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
 }
+
+function flipCard(cardId){
+	console.log("User flipped "+cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+	}
+
+}
+
+flipCard(0);
+flipCard(2);
